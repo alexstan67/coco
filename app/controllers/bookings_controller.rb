10c7e5870546711c35829bde9817952f73d8e5b7
Riza Santoso <rsantoso1001@gmail.com>
2022-05-31 18:13:58 +0800

    bookings controller and view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,12 @@ class BookingsController < ApplicationController
   def create
 
   end
+
+  def index
+    # return a collection with all bookings
+    @bookings = current_user.bookings
+    console
+    # return the first booking
+  end
+
 end
