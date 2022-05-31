7ef42d452d72c275410b4b5743ede970f2c07a3e
Alexandre Stanescot <alex@aerostan.com>
2022-05-31 14:15:47 +0800

    feat: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'faker'
+require 'date'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -9,12 +10,17 @@ require 'faker'
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Cleaning the database..."
+Review.destroy_all
+Booking.destroy_all
+Lesson.destroy_all
 User.destroy_all
 
 puts "Creating users, lessons"
 
 5.times do
   user = User.new
+  user.email = Faker::Internet.email
+  user.password = "123123"
   user.first_name = Faker::Name.first_name
   user.last_name = Faker::Name.last_name
   user.address = Faker::Address.full_address
@@ -22,26 +28,28 @@ puts "Creating users, lessons"
   user.save
   10.times do
     lesson = Lesson.new
-    lesson.teacher_id = user.id
-    hourly_price = 100 + random(20)
-    coding_language = ["ruby", "c", "php", "javascript", "Ruby on Rails"].sample(1)
-    lesson.lesson_description = Faker::Lorem.paragraph(sentence_count: 3)
+    lesson.user_id = user.id
+    lesson.hourly_prices = 100 + rand(20)
+    lesson.coding_language = ["ruby", "c", "php", "javascript", "Ruby on Rails"].sample(1)
+    lesson.description = Faker::Lorem.paragraph(sentence_count: 3)
     lesson.save
     6.times do
       booking = Booking.new
-      booking.user_id = lesson.teacher_id
-      booking.lesson_id = lesson_id
-      booking.duration_min = [60, 90, 120].sample(1)
-      total_price = lesson.hourly_price * booking.duration_min
+      booking.user_id = lesson.user_id
+      booking.lesson_id = lesson.id
+      booking.duration_min = [60, 90, 120].sample
+      booking.total_price = lesson.hourly_prices.to_f * booking.duration_min
+      booking.teaching_date = Date.today()
       booking.comment = Faker::Lorem.paragraph(sentence_count: 3)
       booking.save
       3.times do
         review = Review.new
         review.user_id = booking.user_id
-        rating = [0, 1, 2, 4, 5].sample(1)
+        review.rating = [0, 1, 2, 4, 5].sample
         review.booking_id = booking.id
         review.comment = Faker::Lorem.paragraph(sentence_count: 3)
-      end 
+        review.save
+      end
     end
   end
 end
@@ -49,4 +57,4 @@ end
 puts "#{User.all.count} created!"
 puts "#{Lesson.all.count} created!"
 puts "#{Booking.all.count} created!"
-puts "#{Reveiw.all.count} created!"
+puts "#{Review.all.count} created!"
