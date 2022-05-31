55caab6c359103b34c03f30c661776146a09131e
glen smith <glensmith115@gmail.com>
2022-05-31 12:34:47 +0100

    completed validations for all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :lessons
+  has_many :bookings
+  has_many :reviews
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :phone, :address, presence: true
 end
