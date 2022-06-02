7674fce459eff3dd79f6cec756566b618def2266
Alexandre Stanescot <alex@aerostan.com>
2022-06-02 17:42:08 +0800

    feat: booking creation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,6 +6,7 @@ class BookingsController < ApplicationController
   def index
     # return a collection with all bookings
     @bookings = current_user.teacher_bookings
+    @student_bookings = current_user.bookings
   end
 
   def accept
@@ -27,6 +28,20 @@ class BookingsController < ApplicationController
     else
       flash[:alert] = "Error, try again"
       redirect_to bookings_path
+    end
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.lesson_id = params[:lesson_id]
+    @booking.total_price = (@booking.duration_min.to_f / 60) * @booking.lesson.hourly_prices
+    if @booking.save
+      flash[:alert] = "Booking created!"
+      redirect_to bookings_path
+    else
+      flash[:alert] = "Booking error, please try again!"
+      render "lessons/show"
     end
   end
 
