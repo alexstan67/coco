742812430f5e67ef10bf843db3d19bfd17b601e3
Alexandre Stanescot <alex@aerostan.com>
2022-06-03 13:19:32 +0800

    Total price calculation done

--- app/javascript/controllers/total_price_controller.js
+++ app/javascript/controllers/total_price_controller.js
@@ -1,9 +1,17 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "output" ]
+  static targets = [ "hourlyPrice", "totalPrice" ]
 
   connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+    console.log("connected!");
+  }
+  calculate() {
+    console.log("calculate!");
+    let hourlyPrice = this.hourlyPriceTarget.innerHTML;
+    let duration = event.currentTarget.value / 60;
+    let totalPrice = hourlyPrice * duration;
+    console.log(totalPrice);
+    this.totalPriceTarget.innerHTML = `Total price: ${totalPrice} €`;
   }
 }
